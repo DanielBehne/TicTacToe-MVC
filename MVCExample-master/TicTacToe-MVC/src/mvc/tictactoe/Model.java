8389f367d77fc727bf9a3c521eaee6187917377a
Daniel Behne <daniel.behne@mrjaffesclass.com>
2024-03-11 20:22:36 -0700

    Step 25

--- MVCExample-master/TicTacToe-MVC/src/mvc/tictactoe/Model.java
+++ MVCExample-master/TicTacToe-MVC/src/mvc/tictactoe/Model.java
@@ -36,7 +36,7 @@ public class Model implements MessageHandler {
         this.newGame();
         this.mvcMessaging.subscribe("playerMove", this);
         this.mvcMessaging.subscribe("newGame", this);
-        
+
     }
 
     /**
@@ -54,28 +54,39 @@ public class Model implements MessageHandler {
 
     @Override
     public void messageHandler(String messageName, Object messagePayload) {
+        // Display the message to the console for debugging
         if (messagePayload != null) {
             System.out.println("MSG: received by model: " + messageName + " | " + messagePayload.toString());
         } else {
             System.out.println("MSG: received by model: " + messageName + " | No data sent");
         }
-        MessagePayload payload = (MessagePayload) messagePayload;
-        int field = payload.getField();
-        int direction = payload.getDirection();
 
-        if (direction == Constants.UP) {
-            if (field == 1) {
-                setVariable1(getVariable1() + Constants.FIELD_1_INCREMENT);
-            } else {
-                setVariable2(getVariable2() + Constants.FIELD_2_INCREMENT);
+        // playerMove message handler
+        if (messageName.equals("playerMove")) {
+            // Get the position string and convert to row and col
+            String position = (String) messagePayload;
+            Integer row = new Integer(position.substring(0, 1));
+            Integer col = new Integer(position.substring(1, 2));
+            // If square is blank...
+            if (this.board[row][col].equals("")) {
+                // ... then set X or O depending on whose move it is
+                if (this.whoseMove) {
+                    this.board[row][col] = "X";
+                } else {
+                    this.board[row][col] = "O";
+                }
+                // Send the boardChange message along with the new board 
+                this.mvcMessaging.notify("boardChange", this.board);
             }
-        } else {
-            if (field == 1) {
-                setVariable1(getVariable1() - Constants.FIELD_1_INCREMENT);
-            } else {
-                setVariable2(getVariable2() - Constants.FIELD_2_INCREMENT);
-            }
+
+            // newGame message handler
+        } else if (messageName.equals("newGame")) {
+            // Reset the app state
+            this.newGame();
+            // Send the boardChange message along with the new board 
+            this.mvcMessaging.notify("boardChange", this.board);
         }
+
     }
 
 }
